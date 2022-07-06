fcc839aa538e0813b1faf44b4a180fe5f3f4f71d
chienlx105 <chienlx105@gmail.com>
2022-07-06 09:14:03 +0700

    no

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -1,17 +1,34 @@
 import mongoose from "mongoose";
 
-let userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    minLength: 6,
-    maxLength: 25,
+let userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      minLength: 6,
+      maxLength: 20,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      minLength: 10,
+      maxLength: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 6,
+    },
+    admin: {
+      type: Boolean,
+      default: false,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-    minLength: 8,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
